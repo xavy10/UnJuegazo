d9e53d28c2a9402d2819cb5ddd8223c39f524253
Javier González <javiergzcn@gmail.com>
2018-09-29 22:33:59 -0500

    funciones

--- php/procesa.php
+++ php/procesa.php
@@ -1,4 +1,16 @@
 <?php
   echo "hola esto se cambio";
   echo "Soi moi y me gusta el roger";
+
+  function miFuncion(){
+    echo "esta";
+  }
+
+  function miFuncion1(){
+    echo "esta";
+  }
+
+  function miFuncion2(){
+    echo "esta";
+  }
  ?>
